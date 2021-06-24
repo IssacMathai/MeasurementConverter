7018f165a0e8abc5f2c22b91f79a9b3cf2bce092
IssacMathai <issacmathai@tnt.ac.nz>
2021-06-24 15:08:29 +1200

    V4.5

--- Welcome_Screen_GUI.py
+++ Welcome_Screen_GUI.py
@@ -12,8 +12,15 @@ class Welcome_Screen:
         self.converter_frame.grid()
 
         # Main Heading (row 0)
+        self.measurement_converter_label = Label(self.converter_frame, text = "Measurement Converter",
+            font=("Arial", "20", "bold"),
+            bg=background_colour,
+            padx=10, pady=10)
+        self.measurement_converter_label.grid(row=0)
 
         # Centimetres and Inches Button (row 1)
+        self.centimetres_and_inches_converter_button = Button(self.converter_frame, text="Centimetres and Inches", padx=10, pady=10)
+        self.centimetres_and_inches_converter_button.grid(row=1)
 
         # Metres and Feet Button (row 1)
 
