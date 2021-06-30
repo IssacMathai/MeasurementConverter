ca15299d17b1627149eb22e4895adca0fbe6d830
IssacMathai <issacmathai@tnt.ac.nz>
2021-06-30 16:01:06 +1200

    Initial

---
+++ Help_GUI.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from functools import partial 
+import random
+
+class Help:
+    def __init__(self, parent):
+        # Formatting variables
+        background_colour = "light blue"
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Measurement Converter Tool")
+    something = Help(root)
+    root.mainloop()              
