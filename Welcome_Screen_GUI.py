f21c2e19259efde9851b59f58b4684477e318b2f
IssacMathai <issacmathai@tnt.ac.nz>
2021-06-30 15:55:33 +1200

    Final

--- Welcome_Screen_GUI.py
+++ Welcome_Screen_GUI.py
@@ -11,27 +11,27 @@ class Welcome_Screen:
         m_and_ft_button_colour = "orange"
 
         # Frame
-        self.converter_frame = Frame(width=200, height=200, bg=background_colour, pady=10)
-        self.converter_frame.grid()
+        self.welcome_screen_frame = Frame(width=200, height=200, bg=background_colour, pady=10)
+        self.welcome_screen_frame.grid()
 
         # Main Heading (row 0)
-        self.measurement_converter_label = Label(self.converter_frame, text = "Measurement Converter",
+        self.measurement_welcome_screen_label = Label(self.welcome_screen_frame, text = "Measurement Converter",
             font=("Arial", "20", "bold"),
             bg=background_colour,
             padx=10, pady=10)
-        self.measurement_converter_label.grid(row=0)
+        self.measurement_welcome_screen_label.grid(row=0)
 
-        # Converter buttons frame
-        self.converter_buttons_frame = Frame(self.converter_frame)
-        self.converter_buttons_frame.grid(row=1, pady=10)
+        # Welcome Screen Buttons Frame
+        self.welcome_screen_buttons_frame = Frame(self.welcome_screen_frame)
+        self.welcome_screen_buttons_frame.grid(row=1, pady=10)
 
         # Centimetres and Inches Button (row 1)
-        self.cm_and_in_converter_button = Button(self.converter_buttons_frame, text="Centimetres and Inches", bg=cm_and_in_button_colour, padx=10, pady=10)
-        self.cm_and_in_converter_button.grid(row=1, column=0)
+        self.cm_and_in_welcome_screen_button = Button(self.welcome_screen_buttons_frame, text="Centimetres and Inches", bg=cm_and_in_button_colour, padx=10, pady=10)
+        self.cm_and_in_welcome_screen_button.grid(row=1, column=0)
 
         # Metres and Feet Button (row 1)
-        self.m_and_ft_converter_button = Button(self.converter_buttons_frame, text="Metres and Inches", bg=m_and_ft_button_colour, padx=10, pady=10)
-        self.m_and_ft_converter_button.grid(row=1,column=1)
+        self.m_and_ft_welcome_screen_button = Button(self.welcome_screen_buttons_frame, text="Metres and Inches", bg=m_and_ft_button_colour, padx=10, pady=10)
+        self.m_and_ft_welcome_screen_button.grid(row=1,column=1)
 
 
 
