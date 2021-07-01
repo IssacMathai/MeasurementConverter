911d8f6232401951752d90b75b33c335b3ac6f80
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-02 09:31:56 +1200

    Main Routine

---
+++ Centimetres_Inches_Converter_GUI.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from functools import partial 
+import random
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Measurement Converter Tool")
+    something = Welcome_Screen(root)
+    root.mainloop()
