de870a48c1ce08e5f4c5610b2c55a1ebfa947be2
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-06 11:52:04 +1200

    V1

--- Centimetres_Inches_Converter_GUI.py
+++ Centimetres_Inches_Converter_GUI.py
@@ -2,7 +2,20 @@ from tkinter import *
 from functools import partial 
 import random
 
+class Welcome_Screen:
+    def __init__(self, partner):
+        
+        # Formatting variables...
+        
+        
+        # Converter Main Screen GUI...
+        
 
+        # Temperature Conversion Heading (row 0)
+        
+
+        # Help Button (row 1)
+        
 
 
 # main routine
