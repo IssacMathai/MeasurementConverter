d7c155d4a9ae8850f64eb5ffa7a0661e2cdb3606
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-06 12:09:20 +1200

    GUI Skeleton

--- Centimetres_and_Inches_Converter_GUI.py
+++ Centimetres_and_Inches_Converter_GUI.py
@@ -2,7 +2,20 @@ from tkinter import *
 from functools import partial 
 import random
 
+class Welcome_Screen:
+    def __init__(self, partner):
+        
+        # Formatting variables...
+        
+        
+        # Welcome Screen GUI...
+        
 
+        # Main Heading (row 0)
+        
+
+        # Centimetres and Inches Button (row 1)
+        
 
 
 # main routine
