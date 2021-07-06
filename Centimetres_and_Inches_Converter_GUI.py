a85c58da91373bb7bd851364d18332a7f36b2f0e
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-06 12:17:22 +1200

    V1

--- Centimetres_and_Inches_Converter_GUI.py
+++ Centimetres_and_Inches_Converter_GUI.py
@@ -6,16 +6,19 @@ class Welcome_Screen:
     def __init__(self, partner):
         
         # Formatting variables...
-        
+        background_color = "light blue"
         
         # Welcome Screen GUI...
-        
+        self.welcome_screen_frame = Frame(width=600, height=600, bg=background_color, pady=10)
+        self.welcome_screen_frame.grid()
 
         # Main Heading (row 0)
-        
+        self.measurement_welcome_screen_label = Label(self.welcome_screen_frame, text="Measurement Converter", font=("Arial", "16", "bold"), bg=background_color, padx=10, pady=10)
+        self.measurement_welcome_screen_label.grid(row=0)
 
         # Centimetres and Inches Button (row 1)
-        
+        self.cm_and_in_welcome_screen_button = Button(self.welcome_screen_frame, text="Centimetres and Inches", font=("Arial", "14"), padx=10, pady=10)
+        self.cm_and_in_welcome_screen_button.grid(row=1)
 
 
 # main routine
@@ -24,3 +27,5 @@ if __name__ == "__main__":
     root.title("Measurement Converter Tool")
     something = Welcome_Screen(root)
     root.mainloop()
+
+    
