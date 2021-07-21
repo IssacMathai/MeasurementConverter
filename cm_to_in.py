e840f22543f565e2ee19a62baa5faad4c856d014
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-21 16:28:16 +1200

    function

--- cm_to_in.py
+++ cm_to_in.py
@@ -3,6 +3,13 @@
 measurements = [5, 33.3, 5200]
 converted = []
 
+# Function retrieves a value, converts to inches and returns results
+def to_in(from_cm):
+    inches = (from_cm / 2.54)
+    return inches
+
+
+
 
     
     
