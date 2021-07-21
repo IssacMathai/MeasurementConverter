80fabc597304f0a34f8837a8bf3a238b01c3918a
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-21 16:28:40 +1200

    for loop

--- cm_to_in.py
+++ cm_to_in.py
@@ -8,7 +8,13 @@ def to_in(from_cm):
     inches = (from_cm / 2.54)
     return inches
 
-
+# Retrieves values from 'measurements' list, converts to inches and places results in the 'converted' list
+for item in measurements:
+    result = to_in(item)
+    result_statement = "{} centimetres is {} inches.".format(item,result)
+    converted.append(result_statement)
+    
+print(converted)
 
 
     
