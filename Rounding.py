c3804c8a5275da0ad28963744da84f9c311350c6
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-22 16:49:55 +1200

    use for loop

--- Rounding.py
+++ Rounding.py
@@ -2,5 +2,10 @@
 numbers = [5,33.3333,0.8888,0]
 print(numbers)
 
-
+for item in numbers:
+    try:
+        rounded_number = int(item)
+        print(rounded_number)
+    except ValueError:
+        print("{:.2f}".format(item))
 
