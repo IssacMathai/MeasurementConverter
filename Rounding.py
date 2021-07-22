57abc8230258537cc5ea0a5efad70c7f86254cc5
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-22 16:50:43 +1200

    added comments

--- Rounding.py
+++ Rounding.py
@@ -2,10 +2,12 @@
 numbers = [5,33.3333,0.8888,0]
 print(numbers)
 
+#For each item in list, try converting to integer
 for item in numbers:
     try:
         rounded_number = int(item)
         print(rounded_number)
+# If number cannot be converted to integer, round to 2 dp
     except ValueError:
         print("{:.2f}".format(item))
 
