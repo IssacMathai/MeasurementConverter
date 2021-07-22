9d0dcfdaffd424ff3730d45e602a58729bb23749
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-22 16:46:29 +1200

    added comments

--- Rounding.py
+++ Rounding.py
@@ -1,9 +1,12 @@
+# Input number
 number = input("Enter a number")
 print(number)
 
+# Convert to float
 try:
     new_number = float(number)
     print(new_number)
+# Round up to two decimal places
 except ValueError:
     print("{:.2f}".format(new_number))
 
