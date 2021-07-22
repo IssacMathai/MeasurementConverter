a82219eb5b1f674d82cb61247e272618ffaa3bf3
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-22 19:15:37 +1200

    working rounding

--- Rounding.py
+++ Rounding.py
@@ -2,9 +2,12 @@
 numbers = [5,33.3333,0.8888,0]
 print(numbers)
 
+# For each item in list
 for item in numbers:
-    if item%1 = 0:
+    # If there is no remainder, print to 0dp
+    if item%1 == 0:
         print("{:.0f}".format(item))
+    # Else, print to 2dp
     else:
         print("{:.2f}".format(item))
 
