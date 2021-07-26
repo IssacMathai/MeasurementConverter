5f706dd1c579a9c38971393f4ee8fcb9dec5057d
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-27 10:12:20 +1200

    working first method

---
+++ Store_Conversion_History.py
@@ -0,0 +1,19 @@
+# Empty list
+measurements = []
+
+# User inputs 6 values
+for value in range(0,6):
+    get_value = input("Enter a value.")
+    measurements.append(get_value)
+
+# Reverses list
+measurements.reverse()
+
+# Print whole list
+print("Whole List:")
+print(measurements)
+
+# Print most recent 4 values
+print("Most recent 4 values:")
+for value in range(0,4):
+    print(measurements[value])
