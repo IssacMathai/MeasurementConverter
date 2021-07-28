22b5f9e4a65708ca5438bb53a65d1b6af834902e
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-28 14:49:03 +1200

    added comment

--- Store_Conversion_History.py
+++ Store_Conversion_History.py
@@ -13,4 +13,5 @@ print(measurements)
 # Print most recent 4 values
 print("Most recent 4 values:")
 for value in range(0,4):
+    # Get length of list, print value and subtract 1 so that the next newest item will be printed in the next loop 
     print(measurements[len(measurements) - value - 1])
