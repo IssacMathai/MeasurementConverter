4cacbbdaba41035f92b04affa8e44e2cf685e1e4
IssacMathai <issacmathai@tnt.ac.nz>
2021-07-29 12:49:22 +1200

    no limit on number of values entered

---
+++ Store_Conversion_History_Checker.py
@@ -0,0 +1,23 @@
+# Empty list
+measurements = []
+
+# User inputs values
+get_value = ""
+while get_value !="done":
+    get_value = input("Enter a value:")
+    
+    if get_value == "done":
+        break
+
+    measurements.append(get_value)
+
+
+# Print whole list
+print("Whole List:")
+print(measurements)
+
+# Print most recent 4 values
+print("Most recent 4 values:")
+for value in range(0,4):
+    # Get length of list, print value and subtract 1 so that the next newest item will be printed in the next loop 
+    print(measurements[len(measurements) - value - 1])
