04767d0d6952e15110f0565daa8bb2fa8c75a246
IssacMathai <issacmathai@tnt.ac.nz>
2021-08-02 20:24:19 +1200

    replaced classes and set valid characters and filename

--- Export_Conversion_History.py
+++ Export_Conversion_History.py
@@ -28,6 +28,7 @@ class Centimetres_and_Inches_Converter:
 
         # Formatting variables
         background = "lime green"
+        conversion_history_list = ["8 cm is 3.15 in", "17.7 cm is 6.97 in", "2 in is 5.08 cm", "18.2 in is 46.23 cm", "80 cm is 31.50 in"]
 
         # Disable Centimetres and Inches button while window is open
         partner.cm_and_in_welcome_screen_button.config(state=DISABLED)
@@ -35,7 +36,7 @@ class Centimetres_and_Inches_Converter:
         # Centimetres and Inches Converter child window
         self.cm_and_in_box = Toplevel()
 
-        # Re-enables Centimetres and Inches button if the centimetres and inches converter window is closed using the X button
+        # If users press cross at top, closes window and re-enables Centimetres and Inches button
         self.cm_and_in_box.protocol('WM_DELETE_WINDOW', partial(self.close_Centimetres_and_Inches_Converter, partner))
 
         # GUI Frame
@@ -47,50 +48,91 @@ class Centimetres_and_Inches_Converter:
                                  font="arial 18 bold", bg=background, padx=10, pady=10)
         self.cm_and_in_heading.grid(row=0)
 
-        # conversion_history Button (row 1)
-        self.conversion_history_button = Button(self.cm_and_in_frame, text="Conversion History", font=("Arial 14 bold"), command=self.Conversion_History, padx=10, pady=10)
+        # Conversion History Button
+        self.conversion_history_button = Button(self.cm_and_in_frame, text="Conversion History", font=("Arial", "14"), command=lambda: self.Conversion_History(conversion_history_list), padx=10, pady=10)
         self.conversion_history_button.grid(row=1)
 
+        # If list is empty, print message
+        if len(conversion_history_list) == 0:
+            self.conversion_history_button.config(state=DISABLED)
+
     def close_Centimetres_and_Inches_Converter(self, partner):
-        # Re-enable Centimetres and Inches button in Welcome Screen
+        # Put "Centimetres and Inches" button in welcome screen back to normal... 
         partner.cm_and_in_welcome_screen_button.config(state=NORMAL)
         self.cm_and_in_box.destroy()
 
-    def Conversion_History(self):
-        get_Conversion_History = Conversion_History(self)
+    def Conversion_History(self, conversion_history_list):
+        get_Conversion_History = Conversion_History(self, conversion_history_list)
 
 class Conversion_History:
-    def __init__(self, partner):
-        
+    def __init__(self, partner, conversion_history_list):
+
         # Formatting variables
         ch_background = "#f060f7"
+        export_button_background = "grey"
+        ch_dismiss_button_background = "grey"
+        conversion_history_string = "" 
 
-        # Disable Centimetres and Inches button while window is open
+        # Disable Conversion History button while window is open
         partner.conversion_history_button.config(state=DISABLED)
 
-        # Conversion History child window
+        # Conversion History GUI child window
         self.conversion_history_box = Toplevel()
 
-        # Re-enables Conversion History button if the conversion history window is closed using the X button
+        # If users press cross at top, closes window and re-enables Conversion History button
         self.conversion_history_box.protocol('WM_DELETE_WINDOW', partial(self.close_Conversion_History, partner))
 
         # GUI Frame
-        self.conversion_history_frame = Frame(self.conversion_history_box, bg=ch_background)
+        self.conversion_history_frame = Frame(self.conversion_history_box, width=300, bg=ch_background)
         self.conversion_history_frame.grid()
 
         # Heading (row 0)
-        self.conversion_history_heading = Label(self.conversion_history_frame, text="Conversion History", font="arial 18 bold", bg=ch_background, padx=10, pady=10)
+        self.conversion_history_heading = Label(self.conversion_history_frame, text="Conversion History", font="arial 18 bold", bg=ch_background)
         self.conversion_history_heading.grid(row=0)
 
-        # Export button (row 1)
-        self.export_button = Button(self.conversion_history_frame, text="Export", font="Arial 14 bold", command=self.Export, padx=10, pady=10)
-        self.export_button.grid(row=1)
-    
+        # Instructions text (row 1)
+        self.conversion_history_instructions_text = Label(self.conversion_history_frame, text="Your conversion history will appear below. "
+                                                                                 "In order to export this data onto a text file, "
+                                                                                 "push the export button.", font="Arial 10 italic",
+                                                                                 justify=CENTER, bg=ch_background, wrap=350,
+                                                                                 padx=10, pady=10)
+        self.conversion_history_instructions_text.grid(row=1)
+
+        # Print most recent 5 values
+        if len(conversion_history_list) >=5:
+            for value in range(0,5):
+                # Get length of list, print value and subtract 1 so that the next newest item will be printed in the next loop 
+                conversion_history_string += conversion_history_list[len(conversion_history_list) - value - 1]+ "\n"
+
+        # There are less than 5 values on the list so print what's on the list in order of most recent to least recent
+        else:
+            for value in conversion_history_list:
+                # Get length of list, print value and subtract 1 so that the next newest item will be printed in the next loop
+                conversion_history_string += conversion_history_list[len(conversion_history_list) - conversion_history_list.index(value) - 1] + "\n"
+
+
+        # Placeholder conversion history (row 2)
+        self.conversion_history_values_label = Label(self.conversion_history_frame, text=conversion_history_string,
+                                                    font="Arial 12", bg=ch_background)
+        self.conversion_history_values_label.grid(row=2)
+
+        # Export and Dismiss buttons frames
+        self.export_dismiss_buttons_frame = Frame(self.conversion_history_frame)
+        self.export_dismiss_buttons_frame.grid(row=3)
+
+        # Export button (row 3)
+        self.export_button = Button(self.export_dismiss_buttons_frame, text="Export", font="Arial 12", command=self.Export, bg=export_button_background)
+        self.export_button.grid(row=3)
+
+        # Dismiss button (row 3)
+        self.dismiss_button = Button(self.export_dismiss_buttons_frame, text="Dismiss", font="Arial 12", bg=ch_dismiss_button_background, command=partial(self.close_Conversion_History, partner))
+        self.dismiss_button.grid(row=3, column=1)
+
     def close_Conversion_History(self, partner):
         #  Restore Conversion History button in centimetres and inches converter
         partner.conversion_history_button.config(state=NORMAL)
         self.conversion_history_box.destroy()
-    
+
     def Export(self):
         get_export = Export(self)
 
@@ -133,14 +175,23 @@ class Export:
         self.save_cancel_buttons_frame.grid(row=3, pady=10)
 
         # Save button (row 3)
-        self.save_button = Button(self.save_cancel_buttons_frame, text="Save", font="Arial 13 bold")
+        self.save_button = Button(self.save_cancel_buttons_frame, text="Save", font="Arial 13 bold", command=partial(lambda: self.save_conversion_history(partner)))
         self.save_button.grid(row=3)
 
         # Cancel button (row 3)
         self.cancel_button = Button(self.save_cancel_buttons_frame, text="Cancel", font="Arial 13 bold", command=partial(self.close_export,partner))
         self.cancel_button.grid(row=3, column=2)
 
-        
+    def save_conversion_history(self, partner):
+        # Variables that set valid characters and if an error has been made 
+        valid_characters = "[A-Za-z0-9_]"
+        has_error = "no"
+
+        filename = self.filename_input_box.get()
+        print(filename)
+
+
+
     def close_export(self, partner):
         # Re-enable export button in Conversion History 
         partner.export_button.config(state=NORMAL)
